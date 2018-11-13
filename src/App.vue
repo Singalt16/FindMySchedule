2889c4d3d7171a38f06cee19935c66804d906aa0
singalt16 <root@LAPTOP-GL33FPC0.localdomain>
2018-11-13 09:12:29 -0500

    Added base vue project

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <CourseSelect/>
+  </div>
+</template>
+
+<script>
+import CourseSelect from './components/CourseSelect.vue'
+
+export default {
+  name: 'app',
+  components: {
+    CourseSelect
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/CourseSelect.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <vue-select :options="options"/>
+  </div>
+</template>
+
+<script>
+  import VueSelect from 'vue-select';
+  import Vue from 'vue';
+  Vue.component('v-select', VueSelect.VueSelect);
+
+  let items = [1, 2, 3];
+  export default {
+    name: "course-select",
+    data: function() {
+      return {
+        options: items
+      }
+    },
+    components: { VueSelect }
+  }
+</script>
+
+<style scoped>
+</style>
